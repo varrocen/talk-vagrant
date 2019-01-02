1e00f2bb028505219e27580a48ad9fe97c832d4a
Vincent ARROCENA <varrocen@gmail.com>
2019-01-02 16:07:31 +0100

    Update Vagrantfile and bootstrap.sh

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,6 @@
 Vagrant.configure(2) do |config|
   # Machine Settings
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "bento/ubuntu-18.04"
   # Private Networks
   config.vm.network :forwarded_port, guest: 80, host: 8080
   # Provisioning
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,21 +2,24 @@
 
 # Install Apache and Git
 apt-get update
-apt-get install -y apache2 git
+apt-get install -y apache2 git ruby-full
 if ! [ -L /var/www/html ]; then
   rm -rf /var/www/html
   ln -fs /vagrant /var/www/html
 fi
 
-# Install Asciidoctor
-gem install asciidoctor slim thread_safe tilt
+# Install bundler
+gem install concurrent-ruby bundler
 
-# Install Asciidoctor backend and reveal.js
+# Install the gems into the project
 cd /vagrant
-git clone https://github.com/asciidoctor/asciidoctor-reveal.js.git
-git clone https://github.com/hakimel/reveal.js.git
+bundle config --local github.https true
+bundle --path=.bundle/gems --binstubs=.bundle/.bin
+if ! [ -d reveal.js ]; then
+  git clone -b 3.6.0 --depth 1 https://github.com/hakimel/reveal.js.git
+fi
 
 # Build Vagrant talk
-asciidoctor -T asciidoctor-reveal.js/templates/slim vagrant.adoc
+bundle exec asciidoctor-revealjs vagrant.adoc
 
 exit 0
